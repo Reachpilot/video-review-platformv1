ba79271096ccd588642d7835627eb59fc0dde0d3
Reachpilot <reachpilot.ceo@gmail.com>
2026-02-18 14:11:48 +0100

    thumbnail wortks

--- lib/server/mediaStorage.ts
+++ lib/server/mediaStorage.ts
@@ -53,16 +53,10 @@ const manualBlobOptions: ManualBlobOptions | undefined =
     : undefined;
 
 const hasBlobContext = Boolean(netlifyContext || manualBlobOptions);
-const blobPreference = process.env.USE_BLOB_STORAGE;
-const wantsBlobStorage =
-  blobPreference === 'true'
-    ? true
-    : blobPreference === 'false'
-    ? false
-    : hasBlobContext;
+const wantsBlobStorage = process.env.USE_BLOB_STORAGE === 'true';
 const allowBlobStorage = wantsBlobStorage && hasBlobContext;
 export const staticMediaMode = !allowBlobStorage;
-const requiresBlobStorage = blobPreference === 'true';
+const requiresBlobStorage = wantsBlobStorage;
 const isBlobEnv = allowBlobStorage;
 
 const ensureBlobConfigured = () => {
