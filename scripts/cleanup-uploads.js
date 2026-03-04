18ce21be04c49417def5a929428b0c64f0afac36
Reachpilot <reachpilot.ceo@gmail.com>
2026-03-04 15:57:47 +0100

    Convert scripts to ESM for Netlify build

--- scripts/cleanup-uploads.js
+++ scripts/cleanup-uploads.js
@@ -1,5 +1,10 @@
-const { rm } = require('fs/promises');
-const { join } = require('path');
+import { rm } from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const TARGETS = [
   join(__dirname, '..', 'public', 'uploads'),
