f833cf3c1fe6e64111c4d16888f0468922e4ae7b
Reachpilot <reachpilot.ceo@gmail.com>
2026-03-09 18:30:45 +0100

    ..

--- lib/server/videoRepository.ts
+++ lib/server/videoRepository.ts
@@ -352,4 +352,4 @@ export const deleteVideoRecord = async (videoId: string, segment: VideoSegment =
   await persistStore(store);
 };
 
-export const persistVideo = upsertVideoRecord;
+export const inferSegmentFromParam = (value?: string | null) => normalizeSegment(value);
